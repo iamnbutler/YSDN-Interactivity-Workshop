a41c9de350612573f697ffedd17ed8a3141e3187
Nathan Butler <iamnbutler@gmail.com>
2013-02-27 20:27:04 -0500

    Created week 2 files

---
+++ week_2/week_2_complete/js/scripts.js
@@ -0,0 +1,14 @@
+// Our primary scripts file
+// All of our custom JS and jQuery goes here!
+
+// Regular JS goes here
+
+$(document).ready(function() {
+
+	// jQuery goes inside here!
+	// This makes it so that our scripts don't load
+	// before the page is ready
+	
+	
+
+});
